1dd3bb9621e9b85d1dfbc4360ed049c4507d41be
Vinícius <vinydias0509@gmail.com>
2025-05-11 14:53:24 -0300

    [feat] - Adding a First Reducer (counter) & Store Setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 
 import { AppComponent } from './app/app.component';
+import { counterReducer } from './app/store/counter.reducer';
 
 bootstrapApplication(AppComponent, {
-    providers: [provideStore()]
+    providers: [provideStore({
+        counter: counterReducer,
+    })]
 });
